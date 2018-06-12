5f066e3fa909411a98404fdf979c4b6d66005ff5
rawenfalls <zaqmond@yandex.ru>
2018-06-12 11:48:28 +0300

    v0.12

--- rpn.rb
+++ rpn.rb
@@ -6,44 +6,49 @@ loop do
 
   expression = gets.chomp.strip.scan(/\S/)
   expression.each do |the_element_of_expression|
-    if the_element_of_expression == "^" || the_element_of_expression == "*" || the_element_of_expression == "+"
+    if the_element_of_expression == "^" || the_element_of_expression == "*" || the_element_of_expression == "+" || the_element_of_expression == "/" || the_element_of_expression == "-"
       operand = the_element_of_expression
     else
       output << the_element_of_expression
     end
 
     if operand != 0
-      case operand
-        when "+"
-          loop do
-            loop do
-              break if operands.index("^") == nil
-              output<<operands.delete_at(operands.index("^"))
-            end
-            break if operands.index("*") == nil
-            output<<operands.delete_at(operands.index("*"))
-          end
-          if operands.index("+")!= nil
-            output<<operands.delete_at(operands.index("+"))
-          end
-
-          operands<<operand
-          operand = 0
-        when "*"
-          loop do
-            break if operands.index("^") == nil
+      if operand == "+" || operand == "-"
+        loop do
+          if operands.index("^") != nil
             output<<operands.delete_at(operands.index("^"))
-          end
-          if operands.index("*")!= nil
+          elsif operands.index("*") != nil
             output<<operands.delete_at(operands.index("*"))
+          elsif operands.index("/") != nil
+            output<<operands.delete_at(operands.index("/"))
           end
-          operands<<operand
-          operand = 0
-        when "^"
-          operands<<operand
-          operand = 0
-      end
+          break if operands.index("^") == nil and operands.index("*") == nil and operands.index("/") == nil
+        end
+        if operands.index("+")!= nil
+          output<<operands.delete_at(operands.index("+"))
+        elsif operands.index("-")!= nil
+          output<<operands.delete_at(operands.index("-"))
+        end
+        operands<<operand
+        operand = 0
 
+      elsif operand == "*" || operand == "/"
+        loop do
+          break if operands.index("^") == nil
+          output<<operands.delete_at(operands.index("^"))
+        end
+        if operands.index("*")!= nil
+          output<<operands.delete_at(operands.index("*"))
+        elsif operands.index("/")!= nil
+          output<<operands.delete_at(operands.index("/"))
+        end
+        operands<<operand
+        operand = 0
+
+      else operand == "^"
+      operands<<operand
+      operand = 0
+      end
     end
   end
   loop do
